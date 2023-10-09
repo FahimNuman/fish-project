c98cb1e11cfc9748d65a24dad2a9fe1b17842ff5
Neamul01 <neamulhaque2002@gmail.com>
2023-10-10 00:59:50 +0600

    card padding

--- src/components/Home/FreebieCard.tsx
+++ src/components/Home/FreebieCard.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 export default function FreebieCard({ card }: { card: Props }) {
   return (
-    <div className="bg-white p-6 flex flex-col items-center justify-center gap-3 rounded-2xl">
+    <div className="bg-white p-6 py-14 flex flex-col items-center justify-center gap-3 rounded-2xl">
       <img src={card.icon} alt="icon" className="w-[127px] h-[106px]" />
       <h3 className="uppercase text-base text-black">{card.name}</h3>
     </div>
